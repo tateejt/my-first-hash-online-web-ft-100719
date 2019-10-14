f26ba48ccfd0fb895d68c090163fc6bcebc1bb8f
Emery Tate <tateejt@gmail.com>
2019-10-14 00:26:59 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-  your_hash = {money => 5 , bunny => 23 }
+  your_hash = {"money" => 5 , "bunny" => 23 }
   
 end
 
